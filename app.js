ae49c77678c58b910ef97c160410932491791a54
VinayD <rogerdeadmen@gmail.com>
2020-12-22 20:06:29 +0530

    Update app.js
    minor changes

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-var txt_input = document.querySelector("#txt-input");
-var btn_translate = document.querySelector("#btn-translate");
-var output_div = document.querySelector("#output");
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-var server_URL = "https://api.funtranslations.com/translate/minion.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
 
 function errorHandler(error) {
     console.log("error occured", error);
     alert("something wrong with server! try again after some time")
 }
 
-btn_translate.addEventListener("click", function onClickTranslate() {
 
-    // taking input
-    var input_txt = txt_input.value
-    
-    // alert(input_txt);
-    
-    var translated_URL = server_URL + "?" + "text=" + input_txt
-    
-    // alert(translated_URL);
-    
+function clickHandler() {
+    var inputText = txtInput.value; // taking input
+
     // calling server for processing
-    fetch(translated_URL)
+    fetch(getTranslationURL(inputText))
         .then(response => response.json())
         .then(json => {
-            var translated_txt = json.contents.translated;
-            output_div.innerText - translated_txt;
-            })
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
         .catch(errorHandler)
+};
 
-})
+btnTranslate.addEventListener("click", clickHandler)
